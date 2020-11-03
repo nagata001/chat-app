c9c87635865430a772ffffb534477af0d40629ff
nagata001 <manfree875@gmail.com>
2020-11-03 19:13:56 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_141101) do
+ActiveRecord::Schema.define(version: 2020_11_03_092556) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
